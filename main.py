f734ac5eea112efd7fb270a952b1867618364f5b
Marko Simic <simic10@gmail.com>
2020-02-23 20:56:22 +0900

    Working multiprocessing for pose

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 import argparse
-from multiprocessing import Process, Queue, Manager
+from multiprocessing import Process, Queue, Manager, Pipe
 
 import cv2
 import numpy as np
@@ -17,7 +17,6 @@ from drawables import ChaserBall
 # inference_directory = 'data/pose_output/output'
 # inference_files = sorted([os.path.join(inference_directory, f) for f in os.listdir(inference_directory)])
 
-# cap = cv2.VideoCapture('data/ai_dance_short.MOV')
 
 
 # pose = Pose()
@@ -60,7 +59,7 @@ def set_openpose(args):
     return params
 
 
-def run_openpose(openpose_params):
+def run_openpose(openpose_params, image_pipe, pose_pipe):
     print(openpose_params)
     opWrapper = op.WrapperPython()
     opWrapper.configure(openpose_params)
@@ -69,10 +68,16 @@ def run_openpose(openpose_params):
     datum = op.Datum()
     print("Initialized OpenPose!")
 
+    while True:
+        image = image_pipe.recv()
+        datum.cvInputData = image
+        opWrapper.emplaceAndPop([datum])
+        pose_pipe.send(datum.poseKeypoints)
 
-def initialize_realsense():
-    cv2.namedWindow("projection", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("projection", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+def initialize_realsense(frame_name):
+    cv2.namedWindow(frame_name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(frame_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     # fan_graphics = cv2.imread("/home/ascent/Pictures/fan_diff_small.png")
     stream = cv2.VideoCapture(-1)
     pipe = rs.pipeline()
@@ -80,17 +85,34 @@ def initialize_realsense():
     return stream, pipe, profile
 
 
-def project_visuals():
-    stream, pipe, profile = initialize_realsense()
+def project_visuals(run_live=False,
+                    data_file=None,
+                    frame_name="Frame",
+                    image_pipe=None,
+                    pose_pipe=None):
+    if run_live:
+        stream, pipe, profile = initialize_realsense(frame_name)
+    elif data_file is not None:
+        cap = cv2.VideoCapture(data_file)
+    else:
+        return "Error, running mode not specified properly!"
+
+
     while True:
         frames = pipe.wait_for_frames()
         color_frame = frames.get_color_frame()
         color_image = np.asanyarray(color_frame.get_data())
         color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+        image_pipe.send(color_image)
+        pose = pose_pipe.recv()
+        print(pose)
 
-        cv2.imshow("projection", color_image)
+        cv2.imshow(frame_name, color_image)
+
         key = cv2.waitKey(1)
         if key == ord("q"):
+            stream.release()
+            cv2.destroyAllWindows()
             return "Closing the app, user pressed 'q' key!"
 
 
@@ -142,8 +164,12 @@ if __name__ == "__main__":
     arguments = parse_arguments()
     openpose_params = set_openpose(arguments)
     q = Queue()
-    p = Process(target=run_openpose, args=(openpose_params,))
+    image_pipe_send, image_pipe_receive = Pipe()
+    pose_pipe_send, pose_pipe_receive = Pipe()
+    p = Process(target=run_openpose,
+                daemon=True,
+                args=(openpose_params, image_pipe_receive, pose_pipe_send))
     p.start()
-    message = project_visuals()
+    message = project_visuals(run_live=True, image_pipe=image_pipe_send, pose_pipe=pose_pipe_receive)
     print(message)
     p.join()
