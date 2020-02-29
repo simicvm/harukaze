4af1d8d80a3e02c5e30db77f7ad8fcc54c09f177
Pablo Bordons <pablo.bordons@mercari.com>
2020-02-29 19:19:52 +0900

    adding state machine

--- main_video.py
+++ main_video.py
@@ -115,7 +115,7 @@ def project_visuals(
             time.sleep(0.001)
         
 
-        print("FRAME {}".format(i))
+        # print("FRAME {}".format(i))
         json_path = inference_files[i]
 
         animation.update_pose_from_json(json_path)
@@ -139,7 +139,8 @@ def project_visuals(
             return
 
         calibrator.key_handler(key)
-
+        animation.key_handler(key)
+        # time.sleep(1)
 
 if __name__ == "__main__":
 
